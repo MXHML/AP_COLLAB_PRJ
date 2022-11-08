f1f677850022110e6d48cd5da2b228c5017666df
MXHML <114451098+MXHML@users.noreply.github.com>
2022-11-07 21:38:06 -0600

    Fixed theme button

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -8,8 +8,10 @@ var themeSwitch_btn = document.getElementById("theme-switch");
 function changeTheme(){
     if(themeID==0){
         document.getElementById("html").style.backgroundColor='black';
+        themeID++
     }
     else if(themeID==1){
         document.getElementById("html").style.backgroundColor='white';
+        themeID=0;
     }
 }
