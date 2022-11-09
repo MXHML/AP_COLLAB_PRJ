3cb9ed3e948e71a727a3495b692d2282bc11f57d
MXHML <114451098+MXHML@users.noreply.github.com>
2022-11-09 10:38:36 -0600

    Fixed <p> element overlapping sidenav

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -48,13 +48,13 @@ function changeTheme(){
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     document.getElementById("sideNav").style.width = "250px";
-    document.getElementById("main-container").style.marginLeft = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
   function closeNav() {
     document.getElementById("sideNav").style.width = "0";
-    document.getElementById("main-container").style.marginLeft = "0";
+    document.getElementById("main").style.marginLeft = "0";
   }
 
   
